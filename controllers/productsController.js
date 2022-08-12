322eaf5f7890a53d85acccbbea19faea87163f5b
saraivais <isadorasaraiva95@gmail.com>
2022-08-12 17:49:01 -0300

    fix [productsController]: adds next, fixes arguments & layout

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,37 +2,32 @@ const productsService = require('../services/productsService');
 
 const productsController = {
   
-  getAll: async (_request, response) => {
+  getAll: async (_request, response, _next) => {
     const result = await productsService.getAll();
-
     return response.status(200).json(result);
   },
 
-  getById: async (request, response) => {
+  getById: async (request, response, _next) => {
     const result = await productsService.getById(request.params);
-
     return response.status(200).json(result);
   },
 
-  create: async (request, response) => {
-    const createResult = await productsService.create(request);
+  create: async (request, response, _next) => {
+    const createResult = await productsService.create(request.body);
     return response.status(201).json(createResult);
   },
 
-  edit: async (request, response) => {
-    const id = request.params;
-    const result = await productsService.edit(id, request.body);
-
+  edit: async (request, response, _next) => {
+    const result = await productsService.edit(request.params, request.body);
     return response.status(200).json(result);
   },
 
-  delete: async (request, response) => {
+  delete: async (request, response, _next) => {
     await productsService.delete(request.params);
-
     return response.status(204).end();
   },
 
-  searchByName: async (request, response) => {
+  searchByName: async (request, response, _next) => {
     const result = await productsService.searchByName(request.query);
     return response.status(200).json(result);
   },
