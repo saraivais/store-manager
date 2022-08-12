c8d936537b88459f3a107443e7f5f3d702606415
saraivais <isadorasaraiva95@gmail.com>
2022-08-11 21:28:05 -0300

    test [productsService]: tests exists function~

--- tests/unit/services/productsService.test.js
+++ tests/unit/services/productsService.test.js
@@ -35,7 +35,56 @@ describe('Get all products', () => {
   });
 });
 
-// describe('Exists', () => { });
+describe('Exists', () => {
+
+  describe('Returns true if product exists', () => {
+    before( async () => {
+      const boolean = true;
+
+      sinon.stub(productsModel, 'exists').resolves(boolean);
+    });
+
+    after( async () => {
+      productsModel.exists.restore();
+    });
+
+    it('Returns a boolean', async () => {
+      const result = await productsService.exists({ id: 1 });
+
+      expect(result).to.be.a('boolean');
+    });
+
+    it('The value is true', async () => {
+      const result = await productsService.exists({ id: 1 });
+
+      expect(result).to.be.true;
+    });
+  });
+
+  describe('Return false if product does not exists', () => {
+    before( async () => {
+      const boolean = false;
+
+      sinon.stub(productsModel, 'exists').resolves(boolean);
+    });
+
+    after( async () => {
+      productsModel.exists.restore();
+    });
+
+    it('Returns a boolean', async () => {
+      const result = await productsService.exists({ id: 1001 });
+
+      expect(result).to.be.a('boolean');
+    });
+
+    it('The value is false', async () => {
+      const result = await productsService.exists({ id: 1001 });
+
+      expect(result).to.be.false;
+    });
+  });
+});
 
 // describe('Get product by Id', () => { });
 
