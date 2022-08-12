e3a05ef2d7a92cec201399f2eef2ac6aae205dc9
saraivais <isadorasaraiva95@gmail.com>
2022-08-12 12:14:15 -0300

    feat [productsController]: create middleware implemented~

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,10 +2,21 @@ const productsService = require('../services/productsService');
 
 const productsController = {
   
-  getAll: async (request, response) => {
+  getAll: async (_request, response) => {
     const result = await productsService.getAll();
 
     return response.status(200).json(result);
+  },
+
+  getById: async (request, response) => {
+    const result = await productsService.getById(request.params);
+
+    return response.status(200).json(result);
+  },
+
+  create: async (request, response) => {
+    const createResult = await productsService.create(request);
+    return response.status(201).json(createResult);
   },
 
 };
