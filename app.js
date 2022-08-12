fc0dbf66f19009e367e2c7f80969d5ad6ac34f05
saraivais <isadorasaraiva95@gmail.com>
2022-08-12 17:40:19 -0300

    feat [app]: imports and uses productsRoute~ and uses express.json

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require('express');
+const productsRoute = require('./routes/productsRoute');
+const errorMiddleware = require('./middlewares/errorMiddleware');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+app.use('/products', productsRoute);
+
+app.use(errorMiddleware);
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
