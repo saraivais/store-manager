dbe7585a2418bc449b11eb07fabb0d33aaee2df6
saraivais <isadorasaraiva95@gmail.com>
2022-08-15 15:30:10 -0300

    tests [salesServices]: tests getAll function &~ imports and uses chaiAsPromised for future throw tests

--- tests/unit/services/salesService.test.js
+++ tests/unit/services/salesService.test.js
@@ -1,67 +1,86 @@
-const { expect } = require('chai');
+const chai = require('chai');
+const { expect } = chai;
 const sinon = require('sinon');
 const salesModel = require('../../../models/salesModel');
 const salesService = require('../../../services/salesService');
+const chaiAsPromised = require('chai-as-promised');
 
-describe('#Service - Get all sales', () => {
-  before(async () => { 
-    const execute = [
-      {
-        saleId: 1,
-        date: '2022-08-15 12:23:53',
-        productId: 1,
-        quantity: 5,
-      },
-      {
-        saleId: 1,
-        date: '2022-08-15 12:23:53',
-        productId: 2,
-        quantity: 10,
-      },
-      {
-        saleId: 2,
-        date: '2022-08-15 12:23:53',
-        productId: 3,
-        quantity: 15,
-      },
-    ];
+chai.use(chaiAsPromised);
 
-    sinon.stub(salesModel, 'getAll').resolves(execute);
+describe('Tests salesService', () => {
+  describe('#Service - Get all sales', () => {
+    before(async () => { 
+      const execute = [
+        {
+          saleId: 1,
+          date: '2022-08-15 12:23:53',
+          productId: 1,
+          quantity: 5,
+        },
+        {
+          saleId: 1,
+          date: '2022-08-15 12:23:53',
+          productId: 2,
+          quantity: 10,
+        },
+        {
+          saleId: 2,
+          date: '2022-08-15 12:23:53',
+          productId: 3,
+          quantity: 15,
+        },
+      ];
+  
+      sinon.stub(salesModel, 'getAll').resolves(execute);
+    });
+  
+    after(async () => {
+      salesModel.getAll.restore();
+    });
+  
+    it('Returns an array', async () => {
+      const result = await salesService.getAll();
+  
+      expect(result).to.be.an('array');
+    });
+  
+    it('The array has the expected values', async () => {
+      const result = await salesService.getAll();
+  
+      expect(result).to.be.eql([
+        {
+          saleId: 1,
+          date: '2022-08-15 12:23:53',
+          productId: 1,
+          quantity: 5,
+        },
+        {
+          saleId: 1,
+          date: '2022-08-15 12:23:53',
+          productId: 2,
+          quantity: 10,
+        },
+        {
+          saleId: 2,
+          date: '2022-08-15 12:23:53',
+          productId: 3,
+          quantity: 15,
+        },
+      ]);
+    });
+  
   });
 
-  after(async () => {
-    salesModel.getAll.restore();
-  });
+  describe('#Service - Get sale by Id', () => {
 
-  it('Returns an array', async () => {
-    const result = await salesService.getAll();
+    describe('When the id exists', () => {
 
-    expect(result).to.be.an('array');
-  });
+    });
 
-  it('The array has the expected values', async () => {
-    const result = await salesService.getAll();
+    describe('When the id does not exist', () => {
 
-    expect(result).to.be.eql([
-      {
-        saleId: 1,
-        date: '2022-08-15 12:23:53',
-        productId: 1,
-        quantity: 5,
-      },
-      {
-        saleId: 1,
-        date: '2022-08-15 12:23:53',
-        productId: 2,
-        quantity: 10,
-      },
-      {
-        saleId: 2,
-        date: '2022-08-15 12:23:53',
-        productId: 3,
-        quantity: 15,
-      },
-    ]);
-  });
+    });
+    
+  })
 
 });
