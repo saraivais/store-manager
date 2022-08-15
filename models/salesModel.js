54c82bf2dc8e0a407de4d77571b9238be3c70065
saraivais <isadorasaraiva95@gmail.com>
2022-08-15 09:55:20 -0300

    feat [salesModel]: creates getAll function~

--- models/salesModel.js
+++ models/salesModel.js
@@ -1,8 +1,17 @@
 const connection = require('./connection');
 
 const salesModel = {
-  getAll: async () => { },
-  
+  getAll: async () => {
+    const [result] = await connection.execute(
+      `SELECT sa.id AS saleId, sa.date, sp.product_id AS productId, sp.quantity
+      FROM StoreManager.sales AS sa
+      JOIN StoreManager.sales_products AS sp
+        ON sa.id = sp.sale_id;`,
+    );
+    console.log('result dentro do model', result);
+    return result;
+  },
+
 };
 
 module.exports = salesModel;
