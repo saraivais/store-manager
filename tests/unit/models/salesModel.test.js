48abf3c860615557f74474f96603d6c68b107e73
saraivais <isadorasaraiva95@gmail.com>
2022-08-15 10:27:19 -0300

    test [salesModel]: tests getById function~

--- tests/unit/models/salesModel.test.js
+++ tests/unit/models/salesModel.test.js
@@ -7,26 +7,28 @@ const salesModel = require('../../../models/salesModel');
 describe('#Model - Get all sales', () => {
   describe('When it receives no arguments', () => {
     before(async () => {
-      const execute = [[
-        {
-          saleId: 1,
-          date: '2022-08-15 12:23:53',
-          productId: 1,
-          quantity: 5,
-        },
-        {
-          saleId: 1,
-          date: '2022-08-15 12:23:53',
-          productId: 2,
-          quantity: 10,
-        },
-        {
-          saleId: 2,
-          date: '2022-08-15 12:23:53',
-          productId: 3,
-          quantity: 15,
-        },
-      ]];
+      const execute = [
+        [
+          {
+            saleId: 1,
+            date: '2022-08-15 12:23:53',
+            productId: 1,
+            quantity: 5,
+          },
+          {
+            saleId: 1,
+            date: '2022-08-15 12:23:53',
+            productId: 2,
+            quantity: 10,
+          },
+          {
+            saleId: 2,
+            date: '2022-08-15 12:23:53',
+            productId: 3,
+            quantity: 15,
+          },
+        ]
+      ];
 
       sinon.stub(connection, 'execute').resolves(execute);
     });
@@ -77,6 +79,64 @@ describe('#Model - Get all sales', () => {
   });
 });
 
+// teste do getById()
+describe('#Model - Get sale by Id', () => {
+  describe('When it receives as argument a valid "id"', () => {
+    before(async () => {
+      const execute = [[
+        {
+          date: '2022-08-15 12:23:53',
+          productId: 1,
+          quantity: 5,
+        },
+        {
+          date: '2022-08-15 12:23:53',
+          productId: 2,
+          quantity: 10,
+        }
+      ]];
+
+      sinon.stub(connection, 'execute').resolves(execute);
+    });
+
+    after(async () => {
+      connection.execute.restore();
+    });
+
+    it('Returns an array', async () => {
+      const result = await salesModel.getById(1);
+
+      expect(result).to.be.an('array');
+    });
+
+    it('The objects have "date", "productId" and "quantity" as keys', async () => {
+      const result = await salesModel.getById(1);
+
+      expect(result[0]).to.have.all.keys('date', 'productId', 'quantity');
+      expect(result[1]).to.have.all.keys('date', 'productId', 'quantity');
+    });
+
+    it('Returns the expected values', async () => {
+      const result = await salesModel.getById(1);
+
+      expect(result).to.be.eql([
+        {
+          date: '2022-08-15 12:23:53',
+          productId: 1,
+          quantity: 5,
+        },
+        {
+          date: '2022-08-15 12:23:53',
+          productId: 2,
+          quantity: 10,
+        }
+      ]);
+    });
+    
+  });
+});
+
+
 /*
 [
   {
@@ -97,5 +157,18 @@ describe('#Model - Get all sales', () => {
     productId: 3,
     quantity: 15,
   },
+]
+
+[
+  {
+    date: '2022-08-15 12:23:53',
+    productId: 1,
+    quantity: 5,
+  },
+  {
+    date: '2022-08-15 12:23:53',
+    productId: 2,
+    quantity: 10,
+  }
 ]
 */
