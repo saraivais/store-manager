e93d1ead5b0f54c1a78540360e81681c8dac3dd5
saraivais <isadorasaraiva95@gmail.com>
2022-08-15 16:52:38 -0300

    feat [salesService]: implements productSale validation using Joi~

--- services/salesService.js
+++ services/salesService.js
@@ -1,5 +1,5 @@
-// const Joi = require('joi');
-// const runSchema = require('./schemaValidation');
+const Joi = require('joi');
+const runSchema = require('./schemaValidation');
 const salesModel = require('../models/salesModel');
 
 const salesService = {
@@ -17,7 +17,20 @@ const salesService = {
     const saleById = await salesModel.getById(id);
     return saleById;
   },
-
+  
+  validateProductSaleObject: runSchema(Joi.object({
+    productId: Joi.number().required().empty('')
+      .messages({
+        'any.required': '400|"productId" is required',
+        'any.empty': '400|"productId" is required',
+      }),
+    quantity: Joi.number().required().empty('').min(1)
+      .messages({
+        'any.required': '400|"quantity" is required',
+        'any.empty': '400|"quantity" is required',
+        'number.min': '422|"quantity" must be greater than or equal to 1',
+      }),
+  })),
 };
 
 module.exports = salesService;
